248e70bf9b7e53a281ef95fe37c42ea72fda5944
Jigsaw28 <levenko7@gmail.com>
2024-04-22 14:03:57 +0300

    complete 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector("ul.gallery");
+
+const cards = galleryItems
+  .map(({ original, preview, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", cards);
+
+galleryList.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  const instance = basicLightbox.create(
+    `
+    <img src="${e.target.dataset.source}" />
+`,
+    {
+      onClose: (instance) => {
+        document.removeEventListener("keydown", onCloseModal);
+      },
+      onShow: (instance) => {
+        document.addEventListener("keydown", onCloseModal);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onCloseModal(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+    console.log(event);
+  }
+}
